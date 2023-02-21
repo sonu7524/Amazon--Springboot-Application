f7e48c44f93ee78b7e35114fdd0de71ab4c7a0f8
sonu7524 <sonudaryani135@gmail.com>
2023-02-22 01:14:25 +0530

    Order Service is updated

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -8,14 +8,18 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashSet;
 import java.util.List;
 
-@SpringBootTest(classes = Application.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class TestCases {
+@SpringBootTest
+class TestCases {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
